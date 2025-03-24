d104ec448b43de57b691c53cccf7abd653b51a1e
kzueirf12345 <jaba0192837465@gmail.com>
2025-03-24 23:08:13 +0300

    first commit

---
+++ src/flags/flags.h
@@ -0,0 +1,47 @@
+#ifndef DIFFER_SRC_FLAGS_FLAGS_H
+#define DIFFER_SRC_FLAGS_FLAGS_H
+
+#include "utils/utils.h"
+
+enum FlagsError
+{
+    FLAGS_ERROR_SUCCESS     = 0,
+    FLAGS_ERROR_FAILURE     = 1,
+};
+static_assert(FLAGS_ERROR_SUCCESS  == 0);
+
+const char* flags_strerror(const enum FlagsError error);
+
+#define FLAGS_ERROR_HANDLE(call_func, ...)                                                          \
+    do {                                                                                            \
+        enum FlagsError error_handler = call_func;                                                  \
+        if (error_handler)                                                                          \
+        {                                                                                           \
+            fprintf(stderr, "Can't " #call_func". Error: %s\n",                                     \
+                            flags_strerror(error_handler));                                         \
+            __VA_ARGS__                                                                             \
+            return error_handler;                                                                   \
+        }                                                                                           \
+    } while(0)
+
+enum Mode
+{
+    MODE_NDIFF      = 1,
+    MODE_TAILOR     = 2,
+    MODE_TEST       = 1488
+};
+
+typedef struct FlagsObjs
+{
+    char log_folder  [FILENAME_MAX];
+    
+} flags_objs_t;
+
+enum FlagsError flags_objs_ctor (flags_objs_t* const flags_objs);
+enum FlagsError flags_objs_dtor (flags_objs_t* const flags_objs);
+enum FlagsError flags_processing(flags_objs_t* const flags_objs, 
+                                 const int argc, char* const argv[]);
+
+
+
+#endif /*DIFFER_SRC_FLAGS_FLAGS_H*/
